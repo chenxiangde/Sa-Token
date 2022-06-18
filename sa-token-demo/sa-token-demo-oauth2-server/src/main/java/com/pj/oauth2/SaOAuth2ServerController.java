8ed0c328ea30f0b1b8a5be8cf31091395b614bc0
chenxiangde <xiangdechen@163.com>
2022-06-18 16:57:36 +0800

    代码调整

--- sa-token-demo/sa-token-demo-oauth2-server/src/main/java/com/pj/oauth2/SaOAuth2ServerController.java
+++ sa-token-demo/sa-token-demo-oauth2-server/src/main/java/com/pj/oauth2/SaOAuth2ServerController.java
@@ -59,6 +59,7 @@ public class SaOAuth2ServerController {
     // 全局异常拦截
     @ExceptionHandler
     public SaResult handlerException(Exception e) {
+        e.printStackTrace();
         return SaResult.error(e.getMessage());
     }
 
